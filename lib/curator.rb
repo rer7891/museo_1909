290e8140f008e30b7df162306780335386484b26
Becky Robran <rerobran@gmail.com>
2019-10-30 09:36:51 -0600

    Adds curator file

---
+++ lib/curator.rb
@@ -0,0 +1,16 @@
+class Curator
+  attr_reader :photographs, :artists
+
+  def initialize
+    @photographs = []
+    @artists = []
+  end
+
+  def add_photograph(photo)
+    @photographs << photo
+  end
+
+  def add_artist(artist)
+    @artists << artist
+  end 
+end
