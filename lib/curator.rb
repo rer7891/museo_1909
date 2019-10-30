84a88aba8b8e512b4e8e5f0db3d9563066b42ca5
Becky Robran <rerobran@gmail.com>
2019-10-30 10:39:58 -0600

    Adds photo by country

--- lib/curator.rb
+++ lib/curator.rb
@@ -28,4 +28,20 @@ class Curator
     end
     artists_photos
   end
+
+  def artists_with_multiple_photographs
+    @artists.find do |artist|
+       find_photographs_by_artist(artist).size > 1
+    end
+  end
+
+  def photographs_taken_by_artist_from(country)
+    photos_by_country = []
+    @artists.each do |artist|
+        if artist.country == country
+          photos_by_country << find_photographs_by_artist(artist)
+        end
+      end
+    photos_by_country.flatten
+  end
 end
